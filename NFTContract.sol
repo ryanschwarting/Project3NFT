abdca90f5eb70d9c8b96b7ee37c1004c56903030
Ryan Schwarting <schwartingryan@yahoo.com>
2022-02-02 15:39:35 -0700

    Update NFTContract.sol

--- NFTContract.sol
+++ NFTContract.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract Kaktos is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string public baseURI;
